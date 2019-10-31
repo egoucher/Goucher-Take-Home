2222edbc455abb744a427cdd34d5bf6165434716
egoucher <56981395+egoucher@users.noreply.github.com>
2019-10-31 15:44:32 -0500

    Update R Script.R

---
+++ R Script.R
@@ -0,0 +1,30 @@
+#Preamble----
+rm(list = ls())
+cat("\014")  #sends 'CTRL + L' to console oprional
+
+
+#---------------------------------------------------------------------------------------------------#
+#                                                                                                   #
+#          E. Goucher October 31st 2019 egoucher@tulane.edu                                         #
+#                                                              #                                                                                           #
+#---------------------------------------------------------------------------------------------------#
+
+
+
+library(tidyverse)
+library(devtools)
+
+
+
+date()
+current_date <- date()
+
+
+devtools::session_info()
+sessionInfo()
+
+#Diamonds Data Frame----
+data("diamonds")
+
+#Print first 6 observation ----
+head(diamonds, n=6)
